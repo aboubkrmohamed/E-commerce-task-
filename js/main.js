4ec3c4cb4bb3c10796a4f090211a2819dbf141bb
Aboubkr <aboubkrmohamed86@gmail.com>
2024-04-08 05:12:02 +0200

    task

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+$(document).ready(function() {
+    $('.Join_suppliers_link').click(function() {
+ 
+        
+        // Show the corresponding section
+         $('.Join_suppliers_section').toggleClass('action');
+         $('#search').toggle()
+
+    });
+});
+ 
+
  
