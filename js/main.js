c961bccca7ac6f299c998ca5ab424aaefced392f
Aboubkr <aboubkrmohamed86@gmail.com>
2024-04-16 12:10:46 +0200

    task

--- js/main.js
+++ js/main.js
@@ -1,13 +1,70 @@
+// $(document).ready(function() {
+ 
+
+            
+//     $('.Join_suppliers_link').click(function() {
+
+//         $('.Join_suppliers_section').toggleClass('action');
+//         $('.Trade_Assurance_section').hide()
+
+//        $('#search').hide()
+
+
+       
+
+      
+
+     
+
+//     });
+
+
+
+    
+// });
+ 
+
+ 
+ 
+// $('.Trade_Assurance_link').click(function() {
+
+//     $('.Trade_Assurance_section').toggleClass('action');
+//     $('.Join_suppliers_section').hide()
+//    $('#search').hide()
+
+
+
+ 
+
+// });
+
+
 $(document).ready(function() {
-    $('.Join_suppliers_link').click(function() {
- 
-        
-        // Show the corresponding section
-         $('.Join_suppliers_section').toggleClass('action');
-         $('#search').toggle()
 
-    });
-});
- 
+$('.Join_suppliers_link').click(function(){
+    $('.Join_suppliers_section').toggle();
+    $('#search').toggle()
+})
 
- 
+
+$('.Trade_Assurance_link').click(function(){
+    $('.Trade_Assurance_section').toggle();
+    $('#search').hide()
+})
+
+$('.Customer_center_link').click(function(){
+    $('.Customer_center_section').toggle();
+    $('#search').hide()
+})
+
+$('.solutions_link').click(function(){
+    $('.solutions_section').toggle();
+    $('#search').hide()
+})
+
+$('.get_app_link').click(function(){
+    $('.get_app_section').toggle();
+    $('#search').hide()
+})
+
+})
