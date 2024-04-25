fef6f00a70fea0243ce7fd4a2df31b7cdd2ce066
Aboubkr <aboubkrmohamed86@gmail.com>
2024-04-25 14:38:02 +0200

    task

--- seller dashboard/dashboard pages/Products_dashboard/js/main.js
+++ seller dashboard/dashboard pages/Products_dashboard/js/main.js
@@ -4,9 +4,9 @@ $(document).ready(function() {
     $('#dashboardAction').click(function() {
 
       $('#dashboardAction').toggleClass('dashboardActionAction');
-      $('#dashboard_controller').toggleClass('col-md-2');
-      $('#dashboard_controller').toggleClass('col-md-1');
-      $('#main_section').toggleClass('col-md-11');
+      $('#dashboard_controller').toggleClass('col-sm-2');
+      $('#dashboard_controller').toggleClass('col-sm-1');
+      $('#main_section').toggleClass('col-sm-11');
       $('.dashboard_links_item_span').toggle();
  
     });
