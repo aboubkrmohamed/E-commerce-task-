7945cb8cc4bc937e615107249f22a46b228faca1
Aboubkr <aboubkrmohamed86@gmail.com>
2024-05-08 15:39:56 +0300

    task

---
+++ seller dashboard/dashboard pages/Create_coupon_dashboard/js/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+
+    // Open modal
+    $('.add_url_video_link').click(function() {
+ 
+      $('.dom').show();
+  
+    });
+
+    // close model 
+    $('.closeDom').click(function() {
+ 
+      $('.dom').hide(); 
+    });
+
+
+
+
+    //logo Section 
+
+    $('#logos_link').click(function() {
+ 
+      $('.logos_section').show(); 
+    });
+
+    $('.close_section_logo').click(function () {
+
+      $(".logos_section").hide()
+    })
+
+  });
+  
--- seller dashboard/dashboard pages/sale_dashboard/js/main.js
+++ seller dashboard/dashboard pages/sale_dashboard/js/main.js
@@ -11,9 +11,53 @@ $(document).ready(function() {
     });
  
 
+
+    // Product_condition_item_active
  
+    $('.Product_condition_item').click(function() {
+
+      $('.Product_condition_item').removeClass('Product_condition_item_active');
+        
+      // Add Product_condition_item_active class to the clicked section
+      $(this).addClass('Product_condition_item_active');
+       
+    });
+ 
+
+    // nav controls 
+
+    // order link 
+
+    $('#orderLink').click(function() {
+
+      $('.all').hide();
+
+      $('.order_section').show();
+
+    });
+ 
+
+    // return link 
+
+    $('#returnsLinkk').click(function() {
+
+      $('.all').hide();
+
+      $('.returns_section').show();
+
+    });
     
     
+        // Abandoned Basket Link
+        $('#AbandonedBasketLink').click(function() {
+
+          $('.all').hide();
+    
+          $('.Abandoned_basket').show();
+    
+        });
+        
+        
 
   });
   
