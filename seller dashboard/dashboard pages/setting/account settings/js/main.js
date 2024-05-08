5c28175194fde7ef1a297c54a9c79ab38e8da7ee
Molamei <mo.lamei@nu.edu.eg>
2024-05-08 15:43:11 +0300

    settings
    account settings and main dashboard

---
+++ seller dashboard/dashboard pages/setting/account settings/js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+    // odrers modal
+    $('.nav_control_link').click(function() {
+
+      $('.nav_control_link').removeClass('nav_control_link_active');
+        
+      // Add nav_control_link_active class to the clicked section
+      $(this).addClass('nav_control_link_active');
+       
+    });
+ 
+
+ 
+    
+    
+
+  });
+  
